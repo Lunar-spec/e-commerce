bcd90472503528e7c9759c4d31365b3123594d2f
Manjesh Singh <smanjesh1854@gmail.com>
2023-12-09 10:34:47 +0530

    added: header, footer

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+// import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
+const HeaderComponenet = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href={'/'}>
+          {/* <Image src={'/logo-black.svg'} alt="logo" fill /> */}
+          <h3>The Arctic Cicada</h3>
+        </Link>
+
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponenet
